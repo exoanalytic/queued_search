3ac39f5ee278893313c20de6d7420a15d4cd5535
Taylor Mitchell <taylor.mitchell@gmail.com>
2011-06-17 14:21:30 -0700

    Work around Xapian single-writer restriction

--- queued_search/tasks.py
+++ queued_search/tasks.py
@@ -1,6 +1,11 @@
+import logging
+
+from django.conf import settings
 from haystack import site
 from celery.decorators import task
 from queued_search import utils
+
+logger = logging.getLogger(__name__)
 
 @task(ignore_result=True)
 def add_to_index(obj_identifier, **kwargs):
@@ -9,7 +14,19 @@ def add_to_index(obj_identifier, **kwargs):
     instance = model_class.objects.get(pk=pk)
 
     index = site.get_index(model_class)
-    index.backend.update(index, [instance])
+
+    try:
+        import xapian
+        # work around the single-writer issue in Xapian
+        try:
+            index.backend.update(index, [instance])
+        except xapian.DatabaseLockError, e:
+            retries_remaining = add_to_index.max_retries - kwargs['task_retries']
+            logger.warn("Got DatabaseLockError, will retry %s times", retries_remaining)
+            add_to_index.retry(exc=e)
+
+    except ImportError:
+        index.backend.update(index, [instance])
 
 @task(ignore_result=True)
 def delete_from_index(obj_identifier, **kwargs):
