33b5acbfd894bda73801bd395ad8c262e1a8e0f4
Taylor Mitchell <taylor.mitchell@exoanalytic.com>
2011-12-28 21:44:13 -0800

    Gracefully handle a object we can't find

--- queued_search/tasks.py
+++ queued_search/tasks.py
@@ -1,12 +1,22 @@
+import logging
+
 from haystack import site
 from celery.decorators import task
 from queued_search import utils
+
+
+logger = logging.getLogger(__name__)
+
 
 @task(ignore_result=True)
 def add_to_index(obj_identifier, **kwargs):
     object_path, pk = utils.split_obj_identifier(obj_identifier)
     model_class = utils.get_model_class(object_path)
-    instance = model_class.objects.get(pk=pk)
+    try:
+        instance = model_class.objects.get(pk=pk)
+    except model_class.DoesNotExist:
+        logger.warn("Could not find %s with pk=%s", model_class.__name__, pk)
+        return
 
     index = site.get_index(model_class)
     index.backend.update(index, [instance])
