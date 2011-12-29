3298752a338efa4bb2bf6972d366405cca8c5794
Taylor Mitchell <taylor.mitchell@exoanalytic.com>
2011-12-28 22:26:30 -0800

    Retry after a delay rather than quitting outright
    This can be useful when doing bulk loads within an extended transaction where the objects don't show up right away

--- queued_search/tasks.py
+++ queued_search/tasks.py
@@ -8,15 +8,17 @@ from queued_search import utils
 logger = logging.getLogger(__name__)
 
 
-@task(ignore_result=True)
+@task(ignore_result=True, max_retries=2, default_retry_delay=3600)
 def add_to_index(obj_identifier, **kwargs):
     object_path, pk = utils.split_obj_identifier(obj_identifier)
     model_class = utils.get_model_class(object_path)
     try:
         instance = model_class.objects.get(pk=pk)
-    except model_class.DoesNotExist:
-        logger.warn("Could not find %s with pk=%s", model_class.__name__, pk)
-        return
+    except model_class.DoesNotExist, exc:
+        retries_remaining = add_to_index.max_retries - kwargs['task_retries']
+        logger.warn("Could not find %s with pk=%s, will retry %d more times", model_class.__name__, pk,
+                    retries_remaining)
+        add_to_index.retry(exc=exc)
 
     index = site.get_index(model_class)
     index.backend.update(index, [instance])
